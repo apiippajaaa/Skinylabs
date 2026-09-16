11eb44d0cb1d944639481373debb06dec579e0da
apiippajaaa <afifmisbahuddin7@gmail.com>
2026-09-17 00:01:47 +0700

    refactor

--- app/components/layouts/Container.tsx
+++ app/components/layouts/Container.tsx
@@ -11,7 +11,7 @@ export default function Container({
 }: ContainerProps) {
   return (
     <div
-      className={`mx-auto w-full max-w-7xl px-5 sm:px-6 lg:px-8 xl:px-10 ${className}`}
+      className={`mx-auto w-full max-w-7xl px-5 sm:px-6 lg:px-8 xl:px-10 sm:py-12 py-0 ${className}`}
     >
       {children}
     </div>
